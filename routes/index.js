316960539af04d54a42bde1c9d5f70f6234aa4a2
wmunoa86 <wmunoa@icloud.com>
2026-05-03 13:24:07 -0600

    Add root route to prevent Cannot GET /

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
+router.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
 router.use('/contacts', require('./contacts'));
 
 module.exports = router;
